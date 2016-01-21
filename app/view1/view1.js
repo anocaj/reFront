5eee9d19bad9253abce4df11242777fb93a75dab
alban <alban.nocaj@gmail.com>
2016-01-21 22:57:56 +0100

    First Form with ng-model

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,20 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
+.controller('View1Ctrl', ['$scope', function($scope) {
+    $scope.firstname = {
+        text: 'firstname'
+    }
+    $scope.lastname = {
+        text: 'lastname'
+    }
+    $scope.street = {
+        text: 'street'
+    }
+    $scope.city = {
+        text: 'city'
+    }
+    $scope.zip = {
+        text: 'zip'
+    }
 }]);
